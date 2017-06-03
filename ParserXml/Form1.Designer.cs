9a760e98d8671d6c054f987da83f58d518310580
Antonio Carlos do Nascimento Junior <jr_acn@yahoo.com.br>
2017-06-02 21:14:19 -0300

    init

--- ParserXml/Form1.Designer.cs
+++ ParserXml/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ParserXml
 {
-    partial class Form1
+    partial class formPrincipal
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnCaminho = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtConnectionString = new System.Windows.Forms.TextBox();
+            this.btnGravar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(12, 22);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(349, 20);
+            this.txtPath.TabIndex = 0;
+            // 
+            // btnCaminho
+            // 
+            this.btnCaminho.Location = new System.Drawing.Point(367, 22);
+            this.btnCaminho.Name = "btnCaminho";
+            this.btnCaminho.Size = new System.Drawing.Size(109, 20);
+            this.btnCaminho.TabIndex = 1;
+            this.btnCaminho.Text = "Selecionar Caminho";
+            this.btnCaminho.UseVisualStyleBackColor = true;
+            this.btnCaminho.Click += new System.EventHandler(this.btnCaminho_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Pasta dos arquivos em XML";
+            // 
+            // txtConnectionString
+            // 
+            this.txtConnectionString.Location = new System.Drawing.Point(12, 73);
+            this.txtConnectionString.Name = "txtConnectionString";
+            this.txtConnectionString.Size = new System.Drawing.Size(349, 20);
+            this.txtConnectionString.TabIndex = 0;
+            // 
+            // btnGravar
+            // 
+            this.btnGravar.Location = new System.Drawing.Point(367, 73);
+            this.btnGravar.Name = "btnGravar";
+            this.btnGravar.Size = new System.Drawing.Size(109, 20);
+            this.btnGravar.TabIndex = 1;
+            this.btnGravar.Text = "Gravar";
+            this.btnGravar.UseVisualStyleBackColor = true;
+            this.btnGravar.Click += new System.EventHandler(this.btnGravar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Connection String";
+            // 
+            // formPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(488, 109);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGravar);
+            this.Controls.Add(this.btnCaminho);
+            this.Controls.Add(this.txtConnectionString);
+            this.Controls.Add(this.txtPath);
+            this.Name = "formPrincipal";
+            this.Text = "XML Parser - Firebird";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnCaminho;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtConnectionString;
+        private System.Windows.Forms.Button btnGravar;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- ParserXml/Form1.cs
+++ ParserXml/Form1.cs
@@ -1,20 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace ParserXml
 {
-    public partial class Form1 : Form
+    public partial class formPrincipal : Form
     {
-        public Form1()
+        public formPrincipal()
         {
             InitializeComponent();
+        }
+
+        private void btnCaminho_Click(object sender, EventArgs e)
+        {
+            var x = folderBrowserDialog1.ShowDialog();
+            if (x != DialogResult.OK || String.IsNullOrEmpty(folderBrowserDialog1.SelectedPath))
+                return;
+
+            txtPath.Text = folderBrowserDialog1.SelectedPath;
+        }
+        /// <summary>
+        /// Você vai gerar a classe nesse site -> http://xmltocsharp.azurewebsites.net/
+        /// </summary>
+        public class RootClass
+        {
+            public Person Person { get; set; }
+        }
+        public class Person
+        {
+            public string name { get; set; }
+        }
+
+        private void btnGravar_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(folderBrowserDialog1.SelectedPath))
+                return;
+
+            // Ele já pega somente os arquivos .xml
+            var arquivos = Directory.GetFiles(txtPath.Text, "*.xml");
+            foreach (var arquivo in arquivos)
+            {
+                var xml = new XmlDocument();
+                xml.LoadXml(File.ReadAllText(arquivo));
+                var q = (Newtonsoft.Json.JsonConvert.SerializeObject(xml));
+                //Classe serializada
+                var ee = (Newtonsoft.Json.JsonConvert.DeserializeObject<RootClass>(q));
+            }
         }
     }
 }
